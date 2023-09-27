daad55ee4c5da901dfe7fba708a0fc8e13cf63d8
taylorturton <126579693+taylorturton@users.noreply.github.com>
2023-09-27 10:04:01 +0100

    Add files via upload

---
+++ basic_numpy_array.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create a NumPy array
+my_array = np.array([1, 2, 3, 4, 5])
+
+# Perform some operations
+doubled_array = my_array * 2
+sum_of_elements = np.sum(my_array)
+
+# Print the results
+print("Original Array:", my_array)
+print("Doubled Array:", doubled_array)
+print("Sum of Elements:", sum_of_elements)
